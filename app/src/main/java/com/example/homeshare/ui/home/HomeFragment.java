ae6089325c89c1051d8a7f89b4adaadb9aa99395
Colin Ho <colin.ho99@gmail.com>
2022-11-06 12:59:11 -0800

    refactor

--- app/src/main/java/com/example/homeshare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/homeshare/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.homeshare.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.homeshare.HomeActivity;
+import com.example.homeshare.R;
 import com.example.homeshare.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class HomeFragment extends Fragment {
 
@@ -24,8 +31,25 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        ((HomeActivity)getActivity()).getDb().collection("Invitation")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Get Invitations", document.getId() + " => " + document.getData());
+
+                            }
+                        } else {
+                            Log.d("Get Invitations", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
         return root;
     }
 

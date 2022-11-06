57fae9dcf182f5fdde498e879949febc68cb6222
WilsonLimSet <limsetia@usc.edu>
2022-11-06 12:44:01 -0800

    profile pic reg

--- app/src/main/java/com/example/homeshare/CreateAccountActivity.java
+++ app/src/main/java/com/example/homeshare/CreateAccountActivity.java
@@ -1,15 +1,22 @@
 package com.example.homeshare;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,7 +25,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,14 +39,17 @@ public class CreateAccountActivity extends AppCompatActivity {
     private Button createAccountButton;
     private FirebaseAuth mAuth;
     private FirebaseFirestore mStore;
+    private ImageView profilePic;
     String userID;
     EditText mFullName, mEmail, mPassword, mAbout,mPhone;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         mStore = FirebaseFirestore.getInstance();
         backToLoginButton = findViewById(R.id.backToLoginButton);
@@ -45,6 +59,16 @@ public class CreateAccountActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.createAccountEmailAddress);
         mPassword = findViewById(R.id.createAccountPassword);
         mAbout = findViewById(R.id.createAccountAbout);
+        profilePic = findViewById(R.id.profilePic);
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent,1000);
+            }
+        });
+
 
         backToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,7 +93,35 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
-    public void createAccount(String email, String password, String confirmPassword,String about,String fullname,String phone) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1000){
+            if(resultCode== Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                profilePic.setImageURI(imageUri);
+                uploadImageToFireBase(imageUri);
+            }
+
+        }
+    }
+
+    private void uploadImageToFireBase(Uri imageUri) {
+        StorageReference fileRef = storageReference.child("profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(CreateAccountActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CreateAccountActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void createAccount(String email, String password, String confirmPassword, String about, String fullname, String phone) {
         if (email == null || email.equals("") || password == null || password.equals("")) {
             Toast.makeText(CreateAccountActivity.this, "Email / Password Fields Cannot Be Empty",
                     Toast.LENGTH_LONG).show();
@@ -95,6 +147,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                             user.put("email",email);
                             user.put("fName",fullname);
                             user.put("phone",phone);
+                            user.put("about",about);
                             documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void unused) {
